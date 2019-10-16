ba9c8616c649aaca8a4ed6046066eb7e5b745e6f
BirbaManuel <birba@et.esiea.fr>
2019-10-16 14:13:35 +0200

    [dropdownlist]

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+
+import Menu  from './component/dropdown/Menu';
+import customeDropDown  from './component/customeDropDown/customeDropDown';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="el el_1">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <div className="el el_2">Welcome</div>
-        <div className="el el_3">
-          <div className="element_dropdown_list">dropdownlist</div>
-          <div className="element_input_date">input date</div>
-        </div>
-      </header>
-
-    </div>
+    <Router>
+      <Menu/>
+      <Route exact path="/" render={ (props) => <customeDropDown {...props}/>} />
+    </Router>
   );
 }
 
---
+++ src/component/dropdown/Menu.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import { withRouter } from "react-router";
+import "./Menu.css"
+
+function Menu (props) {
+    const [selected, setSelected] = useState(props.location.pathname.substring(1) || "");
+    // const [selected, setSelected] = useState(props.location.pathname.substring(1) || "");
+
+    const handleClick = (event) => {
+        props.history.push(event.target.value);
+        setSelected(event.target.value);
+
+    };
+
+        return (
+			// <div>dropwdown list</div>
+            <select className="componentMenu_market_select" id="rubrique" value={selected} onChange={handleClick}>
+                <option id="selected" value="/">Choisir un marché</option>
+
+                <optgroup label="Trafic global">
+                    <option value="/">Global</option>
+                </optgroup>
+
+                <optgroup label="Marchés">
+                    <option value="particulier">Particulier</option>
+                    <option value="professionnel">Professionnel</option>
+                    <option value="bp">Banque-privée</option>
+                    <option value="agriculteur">Agriculteur</option>
+                    <option value="association">Association</option>
+                    <option value="collPub">Collectivités publiques</option>
+                    <option value="entreprise">Entreprise</option>
+                </optgroup>
+
+                <optgroup label="Focus">
+                    <option value="assurance">Focus Assurance</option>
+                    <option value="epargne">Focus Epargne</option>
+                    <option value="credits">Focus Crédits</option>
+                </optgroup>
+
+                <optgroup label="Filiales">
+                    <option value="nexecur">Nexecur</option>
+                    <option value="amundi">Amundi</option>
+                    <option value="unimedia">Uni-médias</option>
+                </optgroup>
+            </select>
+        );
+}
+
+export default withRouter(Menu);
+// export default Menu;
